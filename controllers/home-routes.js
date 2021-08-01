3e1bdd9320bd47ce59df0021a61957ca07bfd0dc
Angeleena <angeleenasyse@gmail.com>
2021-08-01 12:44:57 -0700

    handlebars not working

---
+++ controllers/home-routes.js
@@ -0,0 +1,17 @@
+const router = require('express').Router();
+
+// get homepage
+router.get('/', (req, res) => {
+    res.render('homepage', {
+      id: 1,
+      post_url: 'https://handlebarsjs.com/guide/',
+      title: 'Handlebars Docs',
+      created_at: new Date(),
+      comments: [{}, {}],
+      user: {
+        username: 'test_user'
+      }
+    });
+  });
+
+module.exports = router;
